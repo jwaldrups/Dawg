ddc4c6dcbc20ea40e06402be6c95121b57d381c7
John J <7483411+jjakkc@users.noreply.github.com>
2019-09-14 09:04:31 -0400

    Setup basic menu system
    Login button needs implementation still. For now gets directed into start screen.

---
+++ Unity/DogGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    string game_scene = "SampleScene";
+
+    public void PlayGame()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(game_scene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
